3bbfcc057999f2c043b42aaa2530b7d389f2887b
Mark Cafaro <mark.cafaro@gmail.com>
2015-07-03 14:43:42 -0700

    Added ability to skip tests on package and install

--- install.m
+++ install.m
@@ -1,5 +1,9 @@
-function install()
-    package();
+function install(skipTests)
+    if nargin < 1
+        skipTests = false;
+    end
+
+    package(skipTests);
     root = fileparts(mfilename('fullpath'));
     matlab.apputil.install(fullfile(root, 'target', 'Symphony.mlappinstall'));
 end
--- package.m
+++ package.m
@@ -1,5 +1,11 @@
-function package()
-    test();
+function package(skipTests)
+    if nargin < 1
+        skipTests = false;
+    end
+    
+    if ~skipTests
+        test();
+    end
     root = fileparts(mfilename('fullpath'));
     matlab.apputil.package(fullfile(root, 'symphony.prj'));
 end
