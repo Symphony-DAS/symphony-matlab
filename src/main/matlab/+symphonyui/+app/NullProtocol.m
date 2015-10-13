81811cf9c8ba5cd4498c6ec866d1eef4a8c00d61
Mark Cafaro <mark.cafaro@gmail.com>
2015-10-13 12:14:48 -0700

    Removed all property descriptors from null protocol

--- src/main/matlab/+symphonyui/+app/NullProtocol.m
+++ src/main/matlab/+symphonyui/+app/NullProtocol.m
@@ -2,6 +2,10 @@ classdef NullProtocol < symphonyui.core.Protocol
     
     methods
         
+        function d = getPropertyDescriptors(obj) %#ok<MANU>
+            d = symphonyui.core.PropertyDescriptor.empty(0, 1);
+        end
+        
         function [tf, msg] = isValid(obj) %#ok<MANU>
             tf = false;
             msg = [];
