f2573775d3f7c84e5d82c6b4d2cfd8b764a73185
Mark Cafaro <mark.cafaro@gmail.com>
2015-10-20 15:53:28 -0700

    Updated packaging

--- package.m
+++ package.m
@@ -6,6 +6,27 @@ function package(skipTests)
     if ~skipTests
         test();
     end
-    root = fileparts(mfilename('fullpath'));
-    matlab.apputil.package(fullfile(root, 'Symphony.prj'));
+    addpath(genpath(fullfile('src', 'main')));
+    projectFile = fullfile(fileparts(mfilename('fullpath')), 'Symphony.prj');
+    
+    dom = xmlread(projectFile);
+    root = dom.getDocumentElement();
+    config = root.getElementsByTagName('configuration').item(0);
+    
+    version = config.getElementsByTagName('param.version').item(0);
+    version.setTextContent(symphonyui.app.App.version);
+    
+    % FIXME: Not sure why Matlab is including the runtime core requirement.
+    products = config.getElementsByTagName('param.products.name').item(0);
+    items = products.getElementsByTagName('item');
+    for i = 1:items.getLength()
+        item = items.item(i-1);
+        if strcmp(item.getTextContent(), 'MATLAB Runtime - Core')
+            products.removeChild(item);
+        end
+    end
+    
+    xmlwrite(projectFile, dom);
+    
+    matlab.apputil.package(projectFile);
 end
--- src/main/matlab/+symphonyui/+app/App.m
+++ src/main/matlab/+symphonyui/+app/App.m
@@ -7,7 +7,7 @@ classdef App < handle
         end
         
         function v = version()
-            v = '2.0.0-beta.1';
+            v = '2.0.1.1'; % i.e. 2.0-b1
         end
         
         function c = copyright()
