9b7dd7ab0988ae17e12b36fdc3fba54be9facae7
Mark Cafaro <mark.cafaro@gmail.com>
2015-11-04 15:20:23 -0800

    Change build directory to root

--- src/main/matlab/+symphonyui/+app/App.m
+++ src/main/matlab/+symphonyui/+app/App.m
@@ -7,7 +7,7 @@ classdef App < handle
         end
         
         function v = version()
-            v = '2.0.1.1'; % i.e. 2.0-b1
+            v = '2.0.1.2'; % i.e. 2.0-b2
         end
         
         function c = copyright()
