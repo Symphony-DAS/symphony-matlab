840fdc066c1caff2d5c366dfe5646f41a04a8e91
Mark Cafaro <mark.cafaro@gmail.com>
2015-12-11 15:05:45 -0800

    Simplified netreport

--- src/main/matlab/+symphonyui/+core/+util/netReport.m
+++ src/main/matlab/+symphonyui/+core/+util/netReport.m
@@ -3,9 +3,8 @@ function r = netReport(netException)
         netException = NET.NetException('', '', netException);
     end
     x = netException.ExceptionObject;
-    r = char(x.Message);
     while ~isempty(x.InnerException)
         x = x.InnerException;
-        r = [r char(10) char(x.Message)]; %#ok<AGROW>
     end
+    r = char(x.Message);
 end
