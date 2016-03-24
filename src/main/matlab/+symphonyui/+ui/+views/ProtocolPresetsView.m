1c3bcb0530aa11896d2ffcac9de20773af1cd49b
Mark Cafaro <mark.cafaro@gmail.com>
2016-03-24 14:53:47 -0700

    Added new add and remove icons

--- src/main/matlab/+symphonyui/+ui/+views/ProtocolPresetsView.m
+++ src/main/matlab/+symphonyui/+ui/+views/ProtocolPresetsView.m
@@ -1,6 +1,14 @@
 classdef ProtocolPresetsView < appbox.View
     
+    events
+        AddPreset
+        RemovePreset
+    end
+    
     properties (Access = private)
+        presetsTable
+        addButton
+        removeButton
     end
     
     methods
@@ -14,8 +22,35 @@ classdef ProtocolPresetsView < appbox.View
             
             mainLayout = uix.VBox( ...
                 'Parent', obj.figureHandle, ...
-                'Padding', 11, ...
-                'Spacing', 11);
+                'Spacing', 1);
+            
+            obj.presetsTable = appbox.Table( ...
+                'Parent', mainLayout, ...
+                'ColumnName', {'Presets'}, ...
+                'BorderType', 'none', ...
+                'Editable', false);
+            
+            % Presets toolbar.
+            presetsToolbarLayout = uix.HBox( ...
+                'Parent', mainLayout);
+            obj.addButton = Button( ...
+                'Parent', presetsToolbarLayout, ...
+                'Icon', symphonyui.app.App.getResource('icons/add.png'), ...
+                'Callback', @(h,d)notify(obj, 'AddPreset'));
+            obj.removeButton = Button( ...
+                'Parent', presetsToolbarLayout, ...
+                'Icon', symphonyui.app.App.getResource('icons/remove.png'), ...
+                'Callback', @(h,d)notify(obj, 'RemovePreset'));
+            uix.Empty('Parent', presetsToolbarLayout);
+            set(presetsToolbarLayout, 'Widths', [22 22 -1]);
+            
+            set(mainLayout, 'Heights', [-1 22]);
+        end
+        
+        function show(obj)
+            show@appbox.View(obj);
+            drawnow();
+            set(obj.presetsTable, 'ColumnHeaderVisible', false);
         end
         
     end
