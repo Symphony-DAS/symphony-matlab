068a1e1bcf24d0e5085b9b5df64cfb19302ecc03
Mark Cafaro <mark.cafaro@gmail.com>
2016-04-04 11:20:12 -0700

    Fixed case in devices view

--- src/main/matlab/+symphonyui/+ui/+views/DevicesView.m
+++ src/main/matlab/+symphonyui/+ui/+views/DevicesView.m
@@ -31,7 +31,7 @@ classdef DevicesView < appbox.View
             set(obj.figureHandle, ...
                 'Name', 'Devices', ...
                 'Position', screenCenter(500, 300));
-            
+
             mainLayout = uix.VBox( ...
                 'Parent', obj.figureHandle, ...
                 'Padding', 11, ...
@@ -40,20 +40,20 @@ classdef DevicesView < appbox.View
             devicesLayout = uix.HBox( ...
                 'Parent', mainLayout, ...
                 'Spacing', 7);
-            
+
             masterLayout = uix.VBox( ...
                 'Parent', devicesLayout);
-            
+
             obj.deviceListBox = MappedListBox( ...
                 'Parent', masterLayout, ...
                 'Max', 10, ...
                 'Min', 1, ...
                 'Enable', 'off', ...
                 'Callback', @(h,d)notify(obj, 'SelectedDevices'));
-            
+
             detailLayout = uix.VBox( ...
                 'Parent', devicesLayout);
-            
+
             deviceLayout = uix.VBox( ...
                 'Parent', detailLayout, ...
                 'Spacing', 7);
@@ -68,10 +68,10 @@ classdef DevicesView < appbox.View
                 'String', 'Manufacturer:');
             Label( ...
                 'Parent', deviceGrid, ...
-                'String', 'Input Streams:');
+                'String', 'Input streams:');
             Label( ...
                 'Parent', deviceGrid, ...
-                'String', 'Output Streams:');
+                'String', 'Output streams:');
             Label( ...
                 'Parent', deviceGrid, ...
                 'String', 'Background:');
@@ -134,19 +134,19 @@ classdef DevicesView < appbox.View
                 'TooltipString', 'Remove Configuration Setting', ...
                 'Callback', @(h,d)notify(obj, 'RemoveConfigurationSetting'));
             set(configurationToolbarLayout, 'Widths', [-1 22 22]);
-            
+
             set(configurationLayout, 'Heights', [-1 22]);
-            
+
             set(deviceGrid, ...
                 'Widths', [90 -1], ...
                 'Heights', [23 23 23 23 23 -1]);
-            
+
             javacomponent('javax.swing.JSeparator', [], detailLayout);
 
             set(detailLayout, 'Heights', [-1 1]);
-            
+
             set(devicesLayout, 'Widths', [-1 -2]);
-            
+
             % OK control.
             controlsLayout = uiextras.HBox( ...
                 'Parent', mainLayout, ...
@@ -159,7 +159,7 @@ classdef DevicesView < appbox.View
                 'Interruptible', 'off', ...
                 'Callback', @(h,d)notify(obj, 'Ok'));
             set(controlsLayout, 'Sizes', [-1 75]);
-            
+
             set(mainLayout, 'Heights', [-1 23]);
 
             % Set OK button to appear as the default button.
@@ -168,85 +168,85 @@ classdef DevicesView < appbox.View
                 h.setDefaultButton(obj.okButton);
             end
         end
-        
+
         function close(obj)
             close@appbox.View(obj);
             obj.configurationPropertyGrid.Close();
         end
-        
+
         function d = getSelectedDevices(obj)
             d = get(obj.deviceListBox, 'Value');
         end
-        
+
         function enableDeviceList(obj, tf)
             set(obj.deviceListBox, 'Enable', appbox.onOff(tf));
         end
-        
+
         function setDeviceList(obj, names, values)
             set(obj.deviceListBox, 'String', names);
             set(obj.deviceListBox, 'Values', values);
         end
-        
+
         function setName(obj, n)
             set(obj.nameField, 'String', n);
         end
-        
+
         function setManufacturer(obj, m)
             set(obj.manufacturerField, 'String', m);
         end
-        
+
         function setInputStreams(obj, s)
             set(obj.inputStreamsField, 'String', s);
         end
-        
+
         function setOutputStreams(obj, s)
             set(obj.outputStreamsField, 'String', s);
         end
-        
+
         function enableBackground(obj, tf)
             set(obj.backgroundField, 'Enable', appbox.onOff(tf));
         end
-        
+
         function setBackground(obj, b)
             set(obj.backgroundField, 'String', b);
         end
-        
+
         function b = getBackground(obj)
             b = get(obj.backgroundField, 'String');
         end
-        
+
         function setBackgroundUnits(obj, u)
             set(obj.backgroundUnitsField, 'String', u);
         end
-        
+
         function u = getBackgroundUnits(obj)
             u = get(obj.backgroundUnitsField, 'String');
         end
-        
+
         function p = getSelectedConfigurationSetting(obj)
             p = obj.configurationPropertyGrid.GetSelectedProperty();
         end
-        
+
         function setConfiguration(obj, fields)
             set(obj.configurationPropertyGrid, 'Properties', fields);
         end
-        
+
         function updateConfiguration(obj, fields)
             obj.configurationPropertyGrid.UpdateProperties(fields);
         end
-        
+
         function stopEditingConfiguration(obj)
             obj.configurationPropertyGrid.StopEditing();
         end
-        
+
         function enableAddConfigurationSetting(obj, tf)
             set(obj.addConfigurationSettingButton, 'Enable', appbox.onOff(tf));
         end
-        
+
         function enableRemoveConfigurationSetting(obj, tf)
             set(obj.removeConfigurationSettingButton, 'Enable', appbox.onOff(tf));
         end
-        
+
         function requestFigureFocus(obj)
             obj.update();
             jframe = get(handle(obj.figureHandle), 'JavaFrame');
