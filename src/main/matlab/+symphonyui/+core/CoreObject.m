659ddb267627b418ef82a161747c7b8dbf0510c9
Mark Cafaro <mark.cafaro@gmail.com>
2016-04-06 13:19:31 -0700

    Allowed storing null values

--- src/main/matlab/+symphonyui/+core/CoreObject.m
+++ src/main/matlab/+symphonyui/+core/CoreObject.m
@@ -154,7 +154,7 @@ classdef (Abstract) CoreObject < handle
         end
         
         function v = propertyValueFromValue(obj, v) %#ok<INUSL>
-            if iscell(v) || isstruct(v)
+            if iscell(v) || isstruct(v) || isempty(v)
                 v = savejson('', v, 'Compact', true);
             end
         end
