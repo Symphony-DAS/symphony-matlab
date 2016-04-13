a2438d64ce07098419d0345a6338a164eff7f76a
Mark Cafaro <mark.cafaro@gmail.com>
2016-04-13 12:31:09 -0700

    Added some resets to the package script when the GUI screws it up

--- package.m
+++ package.m
@@ -20,8 +20,25 @@ function package(skipTests)
     root = dom.getDocumentElement();
     config = root.getElementsByTagName('configuration').item(0);
     
+    % Update version number.
     version = config.getElementsByTagName('param.version').item(0);
     version.setTextContent(symphonyui.app.App.version);
+    
+    % Replace fullpaths with ${PROJECT_ROOT}.
+    config.setAttribute('file', '${PROJECT_ROOT}\Symphony.prj');
+    config.setAttribute('location', '${PROJECT_ROOT}');
+    output = config.getElementsByTagName('param.output').item(0);
+    output.setTextContent('${PROJECT_ROOT}\target');
+    deliverable = config.getElementsByTagName('build-deliverables').item(0).getElementsByTagName('file').item(0);
+    deliverable.setAttribute('location', '${PROJECT_ROOT}');
+    deliverable.setTextContent('${PROJECT_ROOT}\target');
+    
+    % Remove unsetting the param.output.
+    unsets = config.getElementsByTagName('unset').item(0);
+    param = unsets.getElementsByTagName('param.output');
+    if param.getLength() > 0
+        unsets.removeChild(param.item(0));
+    end
     
     % This adds a new line after each line in the XML
     %xmlwrite(projectFile, dom);
