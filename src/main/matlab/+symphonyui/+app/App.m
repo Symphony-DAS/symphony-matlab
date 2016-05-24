3580284379269473c0fd644bdda72e6c6c23ac40
Mark Cafaro <mark.cafaro@gmail.com>
2016-05-23 18:46:14 -0700

    Updated version number

--- src/main/matlab/+symphonyui/+app/App.m
+++ src/main/matlab/+symphonyui/+app/App.m
@@ -7,7 +7,7 @@ classdef App < handle
         end
 
         function v = version()
-            v = '2.0.2.1'; % i.e. 2.0-rc1
+            v = '2.0.3.0'; % i.e. 2.0-r
         end
 
         function c = copyright()
