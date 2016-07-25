9bb375323d42c58787ad9fa1fa5778ce1c7557c0
Mark Cafaro <mark.cafaro@gmail.com>
2016-07-25 13:52:46 -0700

    Updated version number

--- src/main/matlab/+symphonyui/+app/App.m
+++ src/main/matlab/+symphonyui/+app/App.m
@@ -11,7 +11,7 @@ classdef App < handle
         end
 
         function v = version()
-            v = '2.1.3.2'; % i.e. 2.1-r2
+            v = '2.1.3.3'; % i.e. 2.1-r3
         end
         
         function o = owner()
