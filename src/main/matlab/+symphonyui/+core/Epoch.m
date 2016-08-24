0c9f5bd7d64716bd1428ca1f2fd98642e4c8d974
Mark Cafaro <mark.cafaro@gmail.com>
2016-08-24 14:15:21 -0700

    [closes #9] Epoch duration is now the duration of its longest stimulus OR response

--- src/main/matlab/+symphonyui/+core/Epoch.m
+++ src/main/matlab/+symphonyui/+core/Epoch.m
@@ -18,7 +18,7 @@ classdef Epoch < symphonyui.core.CoreObject
     %
     %   addParameter    - Adds a key/value pair (parameter) to this epoch
     %   addKeyword      - Adds a keyword tage to this epoch
-    
+
     properties
         parameters              % Parameters added to this epoch (containers.Map)
         keywords                % Keywords attached to this epoch
@@ -27,7 +27,7 @@ classdef Epoch < symphonyui.core.CoreObject
     end
 
     properties (SetAccess = private)
-        duration    % Duration of this epoch, defined by the duration of its longest stimulus (duration)
+        duration    % Duration of this epoch, defined by the duration of its longest stimulus or response (duration)
     end
 
     methods
@@ -44,26 +44,26 @@ classdef Epoch < symphonyui.core.CoreObject
 
         function addStimulus(obj, device, stimulus)
             % Adds a stimulus to present when this epoch is run
-            
+
             obj.tryCore(@()obj.cobj.Stimuli.Add(device.cobj, stimulus.cobj));
         end
-        
+
         function removeStimulus(obj, device)
             % Removes a stimulus for the specified device
-            
+
             obj.tryCore(@()obj.cobj.Stimuli.Remove(device.cobj));
-        end                
-        
+        end
+
         function s = getStimulus(obj, device)
-            % Gets the stimulus for the specified device 
-            
+            % Gets the stimulus for the specified device
+
             cstim = obj.tryCoreWithReturn(@()obj.cobj.Stimuli.Item(device.cobj));
             s = symphonyui.core.Stimulus(cstim);
         end
-        
+
         function tf = hasStimulus(obj, device)
             % Indicates if this epoch has a stimulus for the specified device
-            
+
             tf = obj.tryCoreWithReturn(@()obj.cobj.Stimuli.ContainsKey(device.cobj));
         end
 
@@ -71,7 +71,7 @@ classdef Epoch < symphonyui.core.CoreObject
             % A convenience method for adding a constant, zero-frequency, stimulus to this epoch. This method is mostly
             % useful for interval epochs where you just want to set the intervals duration by using a constant mean
             % stimulus.
-            
+
             g = symphonyui.builtin.stimuli.DirectCurrentGenerator();
             g.offset = measurement.quantity;
             g.units = measurement.displayUnits;
@@ -82,26 +82,26 @@ classdef Epoch < symphonyui.core.CoreObject
 
         function addResponse(obj, device)
             % Adds a response to be recorded when this epoch is run
-            
+
             obj.tryCore(@()obj.cobj.Responses.Add(device.cobj, Symphony.Core.Response()));
         end
-        
+
         function removeResponse(obj, device)
             % Removes a response for the specified device
-            
+
             obj.tryCore(@()obj.cobj.Responses.Remove(device.cobj));
         end
 
         function r = getResponse(obj, device)
             % Gets the response for the specified device
-            
+
             cres = obj.tryCoreWithReturn(@()obj.cobj.Responses.Item(device.cobj));
             r = symphonyui.core.Response(cres);
         end
 
         function tf = hasResponse(obj, device)
             % Indicates if this epoch has a response for the specified device
-            
+
             tf = obj.tryCoreWithReturn(@()obj.cobj.Responses.ContainsKey(device.cobj));
         end
 
@@ -111,30 +111,30 @@ classdef Epoch < symphonyui.core.CoreObject
 
         function addParameter(obj, name, value)
             % Adds a key/value pair (parameter) to this epoch
-            
+
             obj.tryCore(@()obj.cobj.ProtocolParameters.Add(name, obj.propertyValueFromValue(value)));
         end
 
         function k = get.keywords(obj)
             k = obj.cellArrayFromEnumerable(obj.cobj.Keywords, @char);
         end
-        
+
         function addKeyword(obj, keyword)
             % Adds a keyword tag to this epoch
-            
+
             obj.tryCore(@()obj.cobj.Keywords.Add(keyword));
         end
 
         function setBackground(obj, device, background)
             % Sets a background value for a device while this epoch is run. A background is only used in the absence of
             % a stimulus.
-            
+
             obj.tryCore(@()obj.cobj.SetBackground(device.cobj, background.cobj, device.cobj.OutputSampleRate));
         end
-        
+
         function tf = hasBackground(obj, device)
             % Indicates if this epoch has a background for the specified device
-            
+
             tf = obj.tryCoreWithReturn(@()obj.cobj.Backgrounds.ContainsKey(device.cobj));
         end
 
