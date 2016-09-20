e8f50c3bf4fbd2df5980bce0a590a6c67fe5dcf4
Mark Cafaro <mark.cafaro@gmail.com>
2016-09-19 17:13:25 -0700

    Added a warning about empty preferred properties on close of data manager

--- src/main/matlab/+symphonyui/+ui/+presenters/MainPresenter.m
+++ src/main/matlab/+symphonyui/+ui/+presenters/MainPresenter.m
@@ -176,8 +176,23 @@ classdef MainPresenter < appbox.Presenter
 
         function tf = closeFile(obj)
             tf = false;
+            
+            experiment = obj.documentationService.getExperiment();
+            entities = [experiment.allSources, experiment.allEpochGroups];
+            n = 0;
+            for i = 1:numel(entities)
+                if any(arrayfun(@(d)d.isPreferred && isempty(d.value), entities{i}.getPropertyDescriptors()))
+                    n = n + 1;
+                end
+            end
+            if n > 0
+                message = [num2str(n) ' entities contain empty preferred properties. Are you sure you want to close the current file?']; 
+            else
+                message = 'Are you sure you want to close the current file?';
+            end
+            
             result = obj.view.showMessage( ...
-                'Are you sure you want to close the current file?', ...
+                message, ...
                 'Close File', ...
                 'Cancel', 'Close');
             if ~strcmp(result, 'Close')
