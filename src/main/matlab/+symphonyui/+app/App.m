624db2eebf34909f1d5e32c6d7313037e43165b1
Mark Cafaro <mark.cafaro@gmail.com>
2016-11-28 15:38:13 -0800

    Preparing for release

--- src/main/matlab/+symphonyui/+app/App.m
+++ src/main/matlab/+symphonyui/+app/App.m
@@ -11,7 +11,7 @@ classdef App < handle
         end
 
         function v = version()
-            v = '2.3.3.0'; % i.e. 2.3-r
+            v = '2.4.3.0'; % i.e. 2.4-r
         end
         
         function o = owner()
